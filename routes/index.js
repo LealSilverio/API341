137323fa01947307384d85f9962cf2ac4e600a18
Sara Leal Silverio <saralealsilverio@gmail.com>
2025-04-25 18:27:50 -0600

    rounting update

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 const router = require('express').Router();
 const indexController = require('../controllers/indexController');
-const contactsRoutes = require('../routes/contacts');
 
 router.get('/', indexController.base);
-router.get('/contacts', contactsRoutes);
 
 module.exports = router;
