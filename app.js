18918ea219e98d926250579d4b6f06dc309fac64
Sara Leal Silverio <saralealsilverio@gmail.com>
2025-04-25 18:35:10 -0600

    week 1 dependencies

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const indexRoutes = require('./routes/index');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 const app = express();
 
 app.use('/', indexRoutes);
