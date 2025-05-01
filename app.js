8cd681946c4e140691a1a93a60638c8c0b0ac0ce
Sara Leal Silverio <saralealsilverio@gmail.com>
2025-05-01 13:01:13 -0600

    fix on week 2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const indexRoutes = require('./routes/index');
 
-const port = process.env.PORT || 3000;
+const port = 8080;
 const app = express();
 
 app.use('/', indexRoutes);
